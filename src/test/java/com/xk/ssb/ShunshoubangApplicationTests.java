edd12c55db328ce1d8e4adf9b3f8ff41d66ce168
xiekai <xiekai@chinazzn.com>
2019-04-14 22:41:39 +0800

    项目框架初步搭建： 2.增加日志配置

--- src/test/java/com/xk/ssb/ShunshoubangApplicationTests.java
+++ src/test/java/com/xk/ssb/ShunshoubangApplicationTests.java
@@ -5,6 +5,8 @@ import com.xk.ssb.constant.UserSexEnum;
 import com.xk.ssb.mapperDao.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,11 +18,13 @@ public class ShunshoubangApplicationTests {
 	@Autowired
 	private UserMapper userMapper;
 
-//	@Test
+	//	@Test
 //	public  void  testInsert(){
 //		User user = new User("xk", "123456789", (byte) 0);
 //		userMapper.insertSelective(user);
 //	}
+	Logger logger = LoggerFactory.getLogger(getClass());
+
 
 	@Test
 	public void get(){
@@ -30,6 +34,12 @@ public class ShunshoubangApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		logger.trace("tarce日志");
+		logger.debug("debug日志");
+		logger.info("info日志");
+		logger.warn("warning日志");
+		logger.error("erro日志");
+
 	}
 
 }
